8a8b20777951fd6d29a5c8c3e493da9a17ee176c
iammuhammadfurqan <sheikhfurqan048@gmail.com>
2025-10-07 11:04:26 +0500

    Initial Commit

---
+++ api/views.py
@@ -0,0 +1,72 @@
+from rest_framework.response import Response
+from api.models import Product, Order
+from api.serializers import ProductInfoSerializer, ProductSerializer, OrderSerializer
+from rest_framework.decorators import api_view
+from rest_framework import generics
+
+# @api_view(['GET'])
+# def product_list(request):
+#     products = Product.objects.all()
+#     serializer = ProductSerializer(products, many=True)
+#     return Response(serializer.data)
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.filter(stock__gt=0)
+    serializer_class = ProductSerializer
+
+# @api_view(['GET'])
+# def product_detail(request, pk):
+#     try:
+#         product = Product.objects.get(pk=pk)
+#     except Product.DoesNotExist:
+#         return Response({"error": "Product not found."}, status=404)
+    
+#     serializer = ProductSerializer(product)
+#     return Response(serializer.data)
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+# @api_view(['GET'])
+# def order_list(request):
+#     order = Order.objects.prefetch_related('items', 'items__product').all()
+#     serializer = OrderSerializer(order, many=True)
+#     return Response(serializer.data)
+
+class OrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items__product').all()
+    serializer_class = OrderSerializer
+
+class UserOrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items__product').all()
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(user=self.request.user)
+    
+    
+    
+# @api_view(['GET'])
+# def product_detail(request):
+#     products = Product.objects.all()
+#     serializer = ProductInfoSerializer({
+#                                         "products": products,
+#         "count": products.count(),
+#         "max_price": max(product.price for product in products) if products else 0
+#     })
+#     return Response(serializer.data)
+
+class ProductDetailInfoAPIView(generics.GenericAPIView):
+    serializer_class = ProductInfoSerializer
+
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all()
+        serializer = self.get_serializer({
+            "products": products,
+            "count": products.count(),
+            "max_price": max(product.price for product in products) if products else 0
+        })
+        return Response(serializer.data)
